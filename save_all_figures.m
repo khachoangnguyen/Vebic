3d9a62ff39eeb50f1ffa6e30f768f076c78f9cf6
nguyenk4 <nguyenk4@T40102-LR001.org.aalto.fi>
2018-06-08 15:10:12 +0300

    abc

--- save_all_figures.m
+++
@@ -1,8 +0,0 @@
-function save_all_figures(fig_name,dir_name,n)
-datetime = datestr(date);
-datetime = strrep(datetime,'-','_');
-figlist=findobj('type','figure'); %find all available figures
-for i = 1:numel(figlist)
-    saveas(figlist(i),fullfile(dir_name,[fig_name '_' num2str(n) '.' num2str(i) '_' datetime '.png']));
-end
-end
